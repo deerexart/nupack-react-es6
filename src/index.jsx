a0ecbe3937856481f4d9421c89bbf50a833171c3
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-07 12:35:54 -0500

    Added a couple of id props

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,18 @@
 import React, {propTypes} from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 
- class CalculateMarkup extends React.Component {
+export default class CalculateMarkup extends React.Component {
   // equivellent var counter = react.creatClass({})
   constructor(props){
     //must pass in props ^^^^
     super(props); // super GIVES US ACCESS TO 'THIS' !! *****
-    this.state = {initialinput:0, withJobMarkup:0, people:0, isFood:false, isFoodMarkup:0, isPharm:false, isPharmMarkup:0, isElectronics:false, isElectronicsMarkup:0};
+    this.state = {initialinput:0, withJobMarkup:0, people:0, isFood:false, isFoodMarkup:0, isPharm:false, isPharmMarkup:0, isElectronics:false, isElectronicsMarkup:0, total:0};
     this.handleInitialCost = this.handleInitialCost.bind(this);
     this.handlePeople = this.handlePeople.bind(this);
     this.checkIfFood = this.checkIfFood.bind(this);
     this.checkIfPharm = this.checkIfPharm.bind(this);
     this.checkIfElectronics = this.checkIfElectronics.bind(this);
+    this.calculateTotal = this.calculateTotal.bind(this);
 
 
   }
@@ -28,8 +29,6 @@ import ReactDOM from 'react-dom';
   }
 
   checkIfFood(props) {
-    // console.log('props food', props);
-    // console.log('props.isFood', this.state.isFood);
     if(this.state.isFood === true){
       this.setState({
         isFoodMarkup: 0,
@@ -42,7 +41,6 @@ import ReactDOM from 'react-dom';
         isFood:!this.state.isFood
       })
     }
-
   }
   checkIfPharm(props) {
     if(this.state.isPharm === true){
@@ -111,7 +109,7 @@ render(){
     <h3>With Job Rate: {parseFloat(this.state.initialinput) * 0.05 + parseFloat(this.state.initialinput)}</h3>
 
     <ul>
-      <li># of People <input type="number" onChange={ this.handlePeople } /></li>
+      <li># of People <input id="people" type="number" onChange={ this.handlePeople } /></li>
       <li><input type="checkbox" id="food" onChange={this.checkIfFood} checked={this.state.isFood}/> Food </li>
       <li><input type="checkbox" id="pharm" onChange={this.checkIfPharm} checked={this.state.isPharm}/> Pharm </li>
       <li><input type="checkbox" id="electronic" onChange={this.checkIfElectronics} checked={this.state.isElectronics}/> Electronics </li>
@@ -120,9 +118,9 @@ render(){
 
     </ul>
 
-    <button onClick={ () => this.calculateTotal(this) }>Calculate</button>
+    <button id="calculate" onClick={ this.calculateTotal }>Calculate</button>
 
-    <h3>Final Cost Estimate: {this.state.total}</h3>
+    <h3 id="markup-total">Final Cost Estimate: {this.state.total}</h3>
 
     <br/>
     <h3>Dynamic Experiment</h3>
@@ -155,10 +153,10 @@ render(){
   )
 }
 }
-
+// export default  CalculateMarkup;
 //
 //
-ReactDOM.render(
-  <CalculateMarkup  />,
-  document.getElementById('app')
-)
+// ReactDOM.render(
+//   <CalculateMarkup  />,
+//   document.getElementById('app')
+// )

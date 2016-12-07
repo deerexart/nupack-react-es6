2a7d6c6ccf191552187256f17d5213b925ee4dfa
Danielle Stiring <danielle@razoredgelabs.com>
2016-12-07 14:19:56 -0500

    Tests for checking existence and if components are defined within Calculate Markup to.have.length & to be defined tests
    	new file:   npm-debug.log
    	modified:   test/calculate-markup.spec.js

--- test/calculate-markup.spec.js
+++ test/calculate-markup.spec.js
@@ -7,12 +7,42 @@ import  CalculateMarkup  from '../src/index.jsx';
 
 
 describe('<CalculateMarkup />', () => {
-
+// Does component exist?
   it('should have 1 div with class markup-calculator', () => {
       const wrapper = mount(<CalculateMarkup/>);
       expect(wrapper.find('.markup-calculator').length).to.equal(1);
 
   });
+/* checking existence annd if things are defined within component */
+
+it('should have 9 children', () => {
+    const wrapper = mount(<CalculateMarkup/>);
+    expect(wrapper.children().length).to.equal(9);
+
+});
+it('should have 3 checkboxes', () =>{
+  const wrapper = mount(<CalculateMarkup />);
+  expect(wrapper.find('input[type="checkbox"]')).to.have.length(3);
+});
+it('should have 2 number inputs', ()=>{
+  const wrapper = mount(<CalculateMarkup />);
+  expect(wrapper.find('input[type="number"]')).to.have.length(2);
+});
+it('should have a button', () => {
+  const wrapper = mount(<CalculateMarkup />);
+  expect(wrapper.find('button')).to.have.length(1);
+});
+it('should have three h3 tags', () => {
+  const wrapper = mount(<CalculateMarkup/>);
+  expect(wrapper.find('h3')).to.have.length(3);
+})
+it('should have states for inital input, withJobMarkup, noOfPeople', () =>{
+  const wrapper = mount(<CalculateMarkup/>);
+  expect(wrapper.state().initialinput).to.be.defined;
+  expect(wrapper.state().withJobMarkup).to.be.defined;
+  expect(wrapper.state().noOfPeople).to.be.defined;
+
+});
 
 
 
